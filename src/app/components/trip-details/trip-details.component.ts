84f2a7f109f224385f8e92edf36b55d0d9b5bd5f
Gokula <gokulakannanit@gmail.com>
2023-10-12 12:17:55 +0530

    initial commit

--- src/app/components/trip-details/trip-details.component.ts
+++ src/app/components/trip-details/trip-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StarRatingColor } from '../star-rating/star-rating.component';
 
 @Component({
@@ -6,12 +6,21 @@ import { StarRatingColor } from '../star-rating/star-rating.component';
   templateUrl: './trip-details.component.html',
   styleUrls: ['./trip-details.component.css']
 })
-export class TripDetailsComponent {
+export class TripDetailsComponent implements OnInit {
   starCount:number = 5;
   rating:number = 3;
   starColor:StarRatingColor = StarRatingColor.warn;
+  timer:number = 5;
 
   onRatingChanged(evt: Event): void {
 
+  }
+
+  ngOnInit(): void{
+    setTimeout(this.stopWatch.bind(this), 60000)
+  }
+  stopWatch(): void {
+    this.timer = this.timer - 1;
+    this.timer > 0 ? setTimeout(this.stopWatch.bind(this), 60000): '';
   }
 }
