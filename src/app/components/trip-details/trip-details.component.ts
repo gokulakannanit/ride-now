8e7419f9fdaf1e069741f7d84f819987d75a7242
Gokula <gokulakannanit@gmail.com>
2023-10-13 14:46:06 +0530

    initial commit

--- src/app/components/trip-details/trip-details.component.ts
+++ src/app/components/trip-details/trip-details.component.ts
@@ -1,6 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { StarRatingColor } from '../star-rating/star-rating.component';
 
+export interface TripDetail {
+  driverName: String,
+  vechileType: String,
+  vechileCompany: String,
+  vechileModel: String,
+  vechileTimer: number,
+  contactNo: String,
+  rating: number,
+  vechileNumber: String,
+  paymentMode: String,
+  approxPrice: number,
+  distance: number,
+  duration: number,
+  totalTrip: number,
+  imageURL: String,
+}
+
 @Component({
   selector: 'app-trip-details',
   templateUrl: './trip-details.component.html',
@@ -8,9 +25,23 @@ import { StarRatingColor } from '../star-rating/star-rating.component';
 })
 export class TripDetailsComponent implements OnInit {
   starCount:number = 5;
-  rating:number = 3;
   starColor:StarRatingColor = StarRatingColor.warn;
-  timer:number = 5;
+  tripDetail: TripDetail = {
+      driverName: 'Chandran L.K.M',
+      vechileType: 'auto',
+      vechileCompany: 'Bajaj',
+      vechileModel: 'LPG 750',
+      vechileTimer: 5,
+      contactNo: '+917358630994',
+      rating: 3,
+      vechileNumber: 'TN14 Z 1234',
+      paymentMode: 'CASH',
+      approxPrice: 120,
+      distance: 12,
+      duration: 23,
+      totalTrip: 124,
+      imageURL: '/assets/images/driver/1.jpg',
+  }
 
   onRatingChanged(evt: Event): void {
 
@@ -20,7 +51,7 @@ export class TripDetailsComponent implements OnInit {
     setTimeout(this.stopWatch.bind(this), 60000)
   }
   stopWatch(): void {
-    this.timer = this.timer - 1;
-    this.timer > 0 ? setTimeout(this.stopWatch.bind(this), 60000): '';
+    this.tripDetail.vechileTimer = this.tripDetail.vechileTimer - 1;
+    this.tripDetail.vechileTimer > 0 ? setTimeout(this.stopWatch.bind(this), 60000): '';
   }
 }
