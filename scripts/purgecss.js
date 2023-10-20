0cfe1f3e05033c417fdd457b021c19b7f41002ad
Gokula <gokulakannanit@gmail.com>
2023-10-20 14:43:08 +0530

    Change in package json

--- scripts/purgecss.js
+++ scripts/purgecss.js
@@ -3,30 +3,30 @@ const fs = require('fs');
 const path = require('path');
 
 // find the styles css file
-const files = getFilesFromPath('./dist', '.css');
+const files = getFilesFromPath('./dist/ride-now', '.css');
 let data = [];
 
 if (!files && files.length <= 0) {
-console.log('cannot find style files to purge');
-return;
+    console.log('cannot find style files to purge');
+    return '';
 }
 
 for (let f of files) {
 // get original file size
-const originalSize = getFilesizeInKiloBytes('./dist/' + f) + 'kb';
+const originalSize = getFilesizeInKiloBytes('./dist/ride-now/' + f) + 'kb';
 var o = { file: f, originalSize: originalSize, newSize: '' };
 data.push(o);
 }
 console.log('Run PurgeCSS...');
 exec(
-'./node_modules/purgecss/bin/purgecss.js -css dist/*.css --content dist/index.html dist/*.js -o dist/',
+'./node_modules/purgecss/bin/purgecss.js -css dist/ride-now/*.css --content dist/ride-now/index.html dist/ride-now/*.js -o dist/ride-now/',
 function (error, stdout, stderr) {
 console.log('PurgeCSS done');
 console.log();
 
 for (let d of data) {
 // get new file size
-const newSize = getFilesizeInKiloBytes('./dist/' + d.file) + 'kb';
+const newSize = getFilesizeInKiloBytes('./dist/ride-now/' + d.file) + 'kb';
 d.newSize = newSize;
 }
 console.table(data);
