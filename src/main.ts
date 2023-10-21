2124be3710d233531dedb5ba84e31f6d906cc6b6
Gokula <gokulakannanit@gmail.com>
2023-10-21 19:56:59 +0530

    changes in compression techniq

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,5 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+//platformBrowserDynamic().bootstrapModule(AppModule, { ngZone: 'noop' }).catch(err => console.error(err));
