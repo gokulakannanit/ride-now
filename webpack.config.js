eb846a1658ef337f5216940df302b6b9cbccf5c2
Gokula <gokulakannanit@gmail.com>
2023-10-21 17:22:51 +0530

    changes in compression techniq

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,20 @@
 const CompressionPlugin = require("compression-webpack-plugin");
-const BrotliPlugin = require("brotli-webpack-plugin");
-const path = require(`path`);
-const zlib = require("zlib");
 
 module.exports = {
   plugins: [
+    new CompressionPlugin({
+      filename: "[path][base].br[query]",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
+      threshold: 0,
+      minRatio: 0.7,
+    }),
     new CompressionPlugin({
       filename: "[path][base].gz[query]",
       algorithm: "gzip",
       test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
       threshold: 0,
       minRatio: 0.7,
-    }),
-    new BrotliPlugin({
-      asset: "[path].br[query]",
-      test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
-      threshold: 0,
-      minRatio: 0.7,
-    }),
+    })
   ],
 };
