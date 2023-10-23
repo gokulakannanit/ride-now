295d6b88de858c167271122e923fb16ae26c5a91
Gokula <gokulakannanit@gmail.com>
2023-10-23 20:27:51 +0530

    ride detail component updated

--- src/app/pages/rides/rides.component.ts
+++ src/app/pages/rides/rides.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+
+interface rideDetail {
+  id: Number,
+  date: String,
+  cabType: 'Sedan' | 'Auto' | 'Mini' | 'Any' | 'Suv',
+  crNo: String,
+  amount: Number,
+}
+
 @Component({
   selector: 'app-rides',
   templateUrl: './rides.component.html',
   styleUrls: ['./rides.component.css']
 })
 export class RidesComponent {
-
+  rideData: rideDetail[] = [
+    {
+      id: 1,
+      date: "Fri, Jun 30, 06:45 AM",
+      cabType: "Sedan",
+      crNo: "CRN 747057",
+      amount: 122
+    },
+    {
+      id: 2,
+      date: "Fri, Jun 30, 06:45 AM",
+      cabType: "Auto",
+      crNo: "CRN 747057",
+      amount: 122
+    },
+    {
+      id: 3,
+      date: "Fri, Jun 30, 06:45 AM",
+      cabType: "Mini",
+      crNo: "CRN 747057",
+      amount: 122
+    }
+  ]
 }
