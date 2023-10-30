06dc0d7c6789641a8e86f63603abb6b22c9d1cff
Gokula <gokulakannanit@gmail.com>
2023-10-30 13:46:03 +0530

    added compression and cache for html

--- server.js
+++ server.js
@@ -2,17 +2,12 @@
 const port = 4000;
 const express = require('express');
 const path = require('path');
-//const spdy = require('spdy')
 const expressStaticGzip = require('express-static-gzip');
 const app = express();
-//const fs = require('fs');
 
-// Use plugin to serve Brotli files if browser supports them or fallback to Gzip
+var compression = require('compression');
 
-/*const options = {
-    key: fs.readFileSync(path.join(__dirname + '/server.key')),
-    cert:  fs.readFileSync(path.join(__dirname + '/server.crt'))
-}*/
+app.use(compression());
 
 app.use("/", expressStaticGzip(path.join(__dirname + '/dist/ride-now'), {
     enableBrotli: true,
@@ -22,25 +17,9 @@ app.use("/", expressStaticGzip(path.join(__dirname + '/dist/ride-now'), {
     }
  }));
 
-// Serve only the static files form the dist directory
-
-//app.use(express.static(__dirname + '/dist/ride-now'));
-
-app.get('/*', function(req,res) {    
+app.get('/*', function(req,res) {   
+    res.setHeader('Cache-Control', 'public, max-age=31536000'); 
     res.sendFile(path.join(__dirname+'/dist/ride-now/index.html'));
 });
 
 app.listen(process.env.PORT || port);
-
-/*
-spdy
-  .createServer(options, app)
-  .listen(process.env.PORT || port, (error) => {
-    if (error) {
-      console.error(error)
-      return process.exit(1)
-    } else {
-      console.log('Listening on port: ' + (process.env.PORT || port))
-    }
-  })
-  */
